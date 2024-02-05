2304dc07196f8fceffa69c19e8148778f0c20e01
Kasi-Code <kasi_bewty@yahoo.com>
2024-02-05 18:56:19 +0000

    Compulsory Task 1 for T21

---
+++ helloWorld.js
@@ -0,0 +1,8 @@
+let greeting = "Hello World"
+const exclamationMark = "!"
+
+console.log(`${greeting}${exclamationMark}`)
+
+greeting = "Git is Awesome"
+
+console.log(`${greeting}${exclamationMark}`)
